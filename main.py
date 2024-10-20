917fcbe1d3dbe05f1c2dbdc673c05dde4f75c14c
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-10-20 20:51:46 +0300

    chore: set up default fastapi app

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+@app.get('/ping')
+async def ping():
+    return {'ping': 'pong'}
