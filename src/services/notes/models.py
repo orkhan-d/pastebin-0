444e1599de9523ff7b574537766c9c137bbc0cd0
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-10-21 00:15:52 +0300

    chore: changed notes model

--- src/services/notes/models.py
+++ src/services/notes/models.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Note(BaseWithUUIDPK, Base):
     __tablename__ = 'notes'
 
-    filename: Mapped[str] = mapped_column()
+    title: Mapped[str] = mapped_column()
     s3_filename: Mapped[str] = mapped_column()
-    uri: Mapped[str] = mapped_column()
+    url: Mapped[str] = mapped_column()
