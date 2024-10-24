b36516433f257cd361a4624a0224a7e0eabab324
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-10-25 01:15:04 +0300

    feat: add generate hash from uuid and backward

--- src/services/notes/utils.py
+++ src/services/notes/utils.py
@@ -1,4 +1,4 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,7 +14,6 @@ async def create_note_db(data: CreateNoteSchema,
     note = Note(
         title=data.title,
         s3_filename=s3_filename,
-        url=str(uuid)
     )
     session.add(note)
     await session.commit()
@@ -26,6 +25,25 @@ async def create_note_db(data: CreateNoteSchema,
 async def get_note_by_url(url: str,
                           session: AsyncSession):
     stmt = (select(Note.title, Note.s3_filename).
-            where(Note.url == url))
+            where(Note.id == generate_id_from_hash(url)))
     result = await session.execute(stmt)
     return result.one_or_none()
+
+
+def generate_hash_from_id(note_id: int):
+    hash = []
+    while note_id > 0:
+        note_id, r = divmod(note_id, 62)
+        hash.append('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[r])
+
+    return ''.join(hash[::-1])
+
+
+def generate_id_from_hash(note_hash: str):
+    num = 0
+    for i, char in enumerate(note_hash):
+        power = len(note_hash) - (i + 1)
+
+        num += '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(char) * 62 ** power
+
+    return UUID(int=num)
